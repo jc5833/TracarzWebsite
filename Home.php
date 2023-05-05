e0e4f42bedadd8fd7156281f7c97381b095f7432
jc5833 <juancisneros358@gmail.com>
2023-05-05 10:46:18 -0700

    Fixing css to homepage

--- Home.php
+++ Home.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>  
-    <link rel="stylesheet" href="Css/Login.css">  
+    <link rel="stylesheet" href="CSS/Login.css">  
     <script src="https://kit.fontawesome.com/805c332ffe.js" crossorigin="anonymous"></script>
     <nav>
         <ul>
@@ -33,21 +33,18 @@
     <div class="homepagefeatures">
         <div class="homepagefeature">
             <span class="featureIcon"><i class="fa-solid fa-wrench"></i></span>
-            <h3 class="featuredescription">Editable stock page</h3>
-            <p class="description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate eveniet, blanditiis sunt quibusdam voluptatibus voluptatem sed
-            illum nihil quas deleniti cum pariatur! Maxime illo itaque debitis officiis, totam fugiat quis?</p>
+            <h3 class="featuredescription">Editable stock</h3>
+            <p class="description"> Be able to add, remove, and update stock whenever necesary</p>
         </div>
         <div class="homepagefeature">
             <span class="featureIcon"><i class="fa-solid fa-lock"></i></span>
             <h3 class="featuredescription">Secure website</h3>
-            <p class="description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate eveniet, blanditiis sunt quibusdam voluptatibus voluptatem sed
-            illum nihil quas deleniti cum pariatur! Maxime illo itaque debitis officiis, totam fugiat quis?</p>
+            <p class="description">Your stock will be safe since it will be tied to an account which will require you to log in everytime</p>
         </div>
         <div class="homepagefeature">
             <span class="featureIcon"><i class="fa-solid fa-check"></i></span>
             <h3 class="featuredescription">Intuitive</h3>
-            <p class="description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate eveniet, blanditiis sunt quibusdam voluptatibus voluptatem sed
-            illum nihil quas deleniti cum pariatur! Maxime illo itaque debitis officiis, totam fugiat quis?</p>
+            <p class="description">Design of the website is meant to make things easier to keep track of your stock no matter of age or technology knowledge</p>
         </div>
     </div>
 </div>
